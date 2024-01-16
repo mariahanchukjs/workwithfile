6540ca98b08b668b79018514a1d005cd8200f4a5
Maria <hanchuk91@gmail.com>
2024-01-16 23:51:12 +0300

    DataCreate

---
+++ logger.py
@@ -0,0 +1,11 @@
+from data_create import name_data(), surname_data(), phone_data(), address_data()
+
+def input_data():
+    name = name_data()
+    surname = surname_data()
+    phone = phone_data()
+    address = address_data()
+
+
+def print_data():
+    pass
